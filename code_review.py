6f898dd1d943bc03e64a8b82844560b9e061704d
Jigar Patel <jpatel@ambrygen.com>
2021-06-24 07:49:38 -0700

    adding sample file

---
+++ code_review.py
@@ -0,0 +1,106 @@
+readInFile = open("/mnt/somedir/Data/sequences.txt", 'r')
+readLines = readInFile.readlines()
+
+stringW = readLines[0].strip()
+stringV = readLines[1].strip()
+
+pam250 = {'A': {'A': 2, 'C': -2, 'E': 0, 'D': 0, 'G': 1, 'F': -3, 'I': -1, 'H': -1, 'K': -1, 'M': -1, 'L': -2, 'N': 0, 'Q': 0, 'P': 1, 'S': 1, 'R': -2, 'T': 1, 'W': -6, 'V': 0, 'Y': -3}, 'C': {'A': -2, 'C': 12, 'E': -5, 'D': -5, 'G': -3, 'F': -4, 'I': -2, 'H': -3, 'K': -5, 'M': -5, 'L': -6, 'N': -4, 'Q': -5, 'P': -3, 'S': 0, 'R': -4, 'T': -2, 'W': -8, 'V': -2, 'Y': 0}, 'E': {'A': 0, 'C': -5, 'E': 4, 'D': 3, 'G': 0, 'F': -5, 'I': -2, 'H': 1, 'K': 0, 'M': -2, 'L': -3, 'N': 1, 'Q': 2, 'P': -1, 'S': 0, 'R': -1, 'T': 0, 'W': -7, 'V': -2, 'Y': -4}, 'D': {'A': 0, 'C': -5, 'E': 3, 'D': 4, 'G': 1, 'F': -6, 'I': -2, 'H': 1, 'K': 0, 'M': -3, 'L': -4, 'N': 2, 'Q': 2, 'P': -1, 'S': 0, 'R': -1, 'T': 0, 'W': -7, 'V': -2, 'Y': -4}, 'G': {'A': 1, 'C': -3, 'E': 0, 'D': 1, 'G': 5, 'F': -5, 'I': -3, 'H': -2, 'K': -2, 'M': -3, 'L': -4, 'N': 0, 'Q': -1, 'P': 0, 'S': 1, 'R': -3, 'T': 0, 'W': -7, 'V': -1, 'Y': -5}, 'F': {'A': -3, 'C': -4, 'E': -5, 'D': -6, 'G': -5, 'F': 9, 'I': 1, 'H': -2, 'K': -5, 'M': 0, 'L': 2, 'N': -3, 'Q': -5, 'P': -5, 'S': -3, 'R': -4, 'T': -3, 'W': 0, 'V': -1, 'Y': 7}, 'I': {'A': -1, 'C': -2, 'E': -2, 'D': -2, 'G': -3, 'F': 1, 'I': 5, 'H': -2, 'K': -2, 'M': 2, 'L': 2, 'N': -2, 'Q': -2, 'P': -2, 'S': -1, 'R': -2, 'T': 0, 'W': -5, 'V': 4, 'Y': -1}, 'H': {'A': -1, 'C': -3, 'E': 1, 'D': 1, 'G': -2, 'F': -2, 'I': -2, 'H': 6, 'K': 0, 'M': -2, 'L': -2, 'N': 2, 'Q': 3, 'P': 0, 'S': -1, 'R': 2, 'T': -1, 'W': -3, 'V': -2, 'Y': 0}, 'K': {'A': -1, 'C': -5, 'E': 0, 'D': 0, 'G': -2, 'F': -5, 'I': -2, 'H': 0, 'K': 5, 'M': 0, 'L': -3, 'N': 1, 'Q': 1, 'P': -1, 'S': 0, 'R': 3, 'T': 0, 'W': -3, 'V': -2, 'Y': -4}, 'M': {'A': -1, 'C': -5, 'E': -2, 'D': -3, 'G': -3, 'F': 0, 'I': 2, 'H': -2, 'K': 0, 'M': 6, 'L': 4, 'N': -2, 'Q': -1, 'P': -2, 'S': -2, 'R': 0, 'T': -1, 'W': -4, 'V': 2, 'Y': -2}, 'L': {'A': -2, 'C': -6, 'E': -3, 'D': -4, 'G': -4, 'F': 2, 'I': 2, 'H': -2, 'K': -3, 'M': 4, 'L': 6, 'N': -3, 'Q': -2, 'P': -3, 'S': -3, 'R': -3, 'T': -2, 'W': -2, 'V': 2, 'Y': -1}, 'N': {'A': 0, 'C': -4, 'E': 1, 'D': 2, 'G': 0, 'F': -3, 'I': -2, 'H': 2, 'K': 1, 'M': -2, 'L': -3, 'N': 2, 'Q': 1, 'P': 0, 'S': 1, 'R': 0, 'T': 0, 'W': -4, 'V': -2, 'Y': -2}, 'Q': {'A': 0, 'C': -5, 'E': 2, 'D': 2, 'G': -1, 'F': -5, 'I': -2, 'H': 3, 'K': 1, 'M': -1, 'L': -2, 'N': 1, 'Q': 4, 'P': 0, 'S': -1, 'R': 1, 'T': -1, 'W': -5, 'V': -2, 'Y': -4}, 'P': {'A': 1, 'C': -3, 'E': -1, 'D': -1, 'G': 0, 'F': -5, 'I': -2, 'H': 0, 'K': -1, 'M': -2, 'L': -3, 'N': 0, 'Q': 0, 'P': 6, 'S': 1, 'R': 0, 'T': 0, 'W': -6, 'V': -1, 'Y': -5}, 'S': {'A': 1, 'C': 0, 'E': 0, 'D': 0, 'G': 1, 'F': -3, 'I': -1, 'H': -1, 'K': 0, 'M': -2, 'L': -3, 'N': 1, 'Q': -1, 'P': 1, 'S': 2, 'R': 0, 'T': 1, 'W': -2, 'V': -1, 'Y': -3}, 'R': {'A': -2, 'C': -4, 'E': -1, 'D': -1, 'G': -3, 'F': -4, 'I': -2, 'H': 2, 'K': 3, 'M': 0, 'L': -3, 'N': 0, 'Q': 1, 'P': 0, 'S': 0, 'R': 6, 'T': -1, 'W': 2, 'V': -2, 'Y': -4}, 'T': {'A': 1, 'C': -2, 'E': 0, 'D': 0, 'G': 0, 'F': -3, 'I': 0, 'H': -1, 'K': 0, 'M': -1, 'L': -2, 'N': 0, 'Q': -1, 'P': 0, 'S': 1, 'R': -1, 'T': 3, 'W': -5, 'V': 0, 'Y': -3}, 'W': {'A': -6, 'C': -8, 'E': -7, 'D': -7, 'G': -7, 'F': 0, 'I': -5, 'H': -3, 'K': -3, 'M': -4, 'L': -2, 'N': -4, 'Q': -5, 'P': -6, 'S': -2, 'R': 2, 'T': -5, 'W': 17, 'V': -6, 'Y': 0}, 'V': {'A': 0, 'C': -2, 'E': -2, 'D': -2, 'G': -1, 'F': -1, 'I': 4, 'H': -2, 'K': -2, 'M': 2, 'L': 2, 'N': -2, 'Q': -2, 'P': -1, 'S': -1, 'R': -2, 'T': 0, 'W': -6, 'V': 4, 'Y': -2}, 'Y': {'A': -3, 'C': 0, 'E': -4, 'D': -4, 'G': -5, 'F': 7, 'I': -1, 'H': 0, 'K': -4, 'M': -2, 'L': -1, 'N': -2, 'Q': -4, 'P': -5, 'S': -3, 'R': -4, 'T': -3, 'W': 0, 'V': -2, 'Y': 10}}
+indelValue = 5
+
+
+"""
+ Find the highest-scoring local alignment between two strings.
+Given: Two amino acid strings.
+Return: The maximum score of a local alignment of the strings, followed by a local alignment of
+ these strings achieving the maximum score. Use the PAM250 scoring matrix and indel penalty σ = 5.
+ (If multiple local alignments achieving the maximum score exist, you may return any one.)
+ Input: 
+ MEANLY
+ PENALTY
+ Output: 
+ 15
+ EANL-Y
+ ENALTY
+"""
+def localAlignment(stringOne, stringTwo):
+    scoreMatrix = [[0 for col in range(len(stringOne)+1)] for row in range(len(stringTwo)+1)]
+    backTrack = [[0 for col in range(len(stringOne)+1)] for row in range(len(stringTwo)+1)]
+    maxScoreKeeper = (0, (0,0))
+
+    '''#intializes the matrix with use of indelValue
+    counter = 1
+    for row in range(1,len(scoreMatrix)):
+        scoreMatrix[row][0] = -1*(indelValue * counter)
+        counter+=1
+    counter = 1
+    for col in range(1,len(scoreMatrix[0])):
+        scoreMatrix[0][col] = -1*(indelValue * counter)
+        counter+=1
+    '''
+
+    for row in range(1,len(scoreMatrix)):
+        for col in range(1,len(scoreMatrix[row])):
+
+            #gets the value of deletion, insertion and match/mistmatch
+            deletion = (scoreMatrix[row-1][col] )- indelValue #down
+            insertion = (scoreMatrix[row][col-1])-indelValue #insertion
+            match = (scoreMatrix[row-1][col-1] + pam250[stringOne[col-1]][stringTwo[row-1]])
+
+            #selects the max of the three
+            maxSC= max(deletion, insertion, match,0)
+            #assigns it to the index
+            scoreMatrix[row][col] = maxSC
+
+            #keeps track of maximum score in the matrix
+            if(maxSC > maxScoreKeeper[0]):
+                maxScoreKeeper = (maxSC, (row,col))
+
+            #records the direction
+            if(scoreMatrix[row][col] == 0):
+                backTrack[row][col] = 4
+            elif(scoreMatrix[row][col] == deletion):
+                backTrack[row][col] = 1
+            elif(scoreMatrix[row][col] == insertion):
+                backTrack[row][col] = 2
+            else:
+                backTrack[row][col] = 3
+
+
+    return backTrack, scoreMatrix, maxScoreKeeper
+
+
+finalSOne = []
+finalSTwo = []
+########################################################################
+# Used to bracktrack longest path between two strings
+########################################################################
+def followBack(backTrack, scoreMatrix, stringOne, stringTwo, row, col):
+    if(row <= 1 or col < 1):
+        return
+
+    #stops where the taxi started
+    if(backTrack[row][col] == 4):
+        return
+    elif(backTrack[row][col] == 1):
+        followBack(backTrack, scoreMatrix, stringOne, stringTwo, row-1, col)
+        finalSOne.append("-")
+        finalSTwo.append(stringTwo[row-1])
+    elif(backTrack[row][col] == 2):
+        followBack(backTrack, scoreMatrix, stringOne, stringTwo, row, col-1)
+        finalSOne.append(stringOne[col - 1])
+        finalSTwo.append("-")
+    else:
+        followBack(backTrack, scoreMatrix, stringOne, stringTwo, row-1, col-1)
+        finalSOne.append(stringOne[col-1])
+        finalSTwo.append(stringTwo[row-1])
+
+
+#calls the global alignment algorithm
+backT, scoreM, coordinate = localAlignment(stringW, stringV)
+frow = coordinate[1][0]
+fcol = coordinate[1][1]
+
+
+followBack(backT, scoreM,stringW, stringV,frow, fcol)
+
+print(coordinate[0],finalSOne,finalSTwo)
